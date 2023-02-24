3eae73954c70b5cc4f2201a85f07d96b492bd065
Valentin Chanas <anisometropie@gmail.com>
2023-02-24 19:47:52 +0100

    looping and comparing guess to secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 
@@ -9,15 +10,28 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
     
-    println!("Please input your guess.");
-
-    let mut guess = String::new(); // new is an associated function of the String type
-    // what happens when you bind a new value? is it a new reference, or is it the same string reference that’s being changed?
-    io::stdin() // stdin function from io module. returns an instance of std::io::Stdin (a handle to the standard input)
-        .read_line(&mut guess) // method of the instance. reference to guess. references are immutable by default
-        .expect("Failed to read line"); // read_line returns a Result value (an enum)
-    // if the value is an Err, it will crash the program
-    // if the value is an Ok, it will return the value held inside
-
-    println!("You guessed: {guess}");
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new(); // new is an associated function of the String type
+        // what happens when you bind a new value? is it a new reference, or is it the same string reference that’s being changed?
+        io::stdin() // stdin function from io module. returns an instance of std::io::Stdin (a handle to the standard input)
+            .read_line(&mut guess) // method of the instance. reference to guess. references are immutable by default
+            .expect("Failed to read line"); // read_line returns a Result value (an enum)
+        // if the value is an Err, it will crash the program
+        // if the value is an Ok, it will return the value held inside
+    
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
